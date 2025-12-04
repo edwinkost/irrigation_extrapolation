7f3848470fa69cba74f3d0232491d8b4f10faa62
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2025-12-04 11:03:12 +0100

    start working

---
+++ scripts/irr_extrapolate.py
@@ -0,0 +1,12 @@
+
+basin_map = 
+basin_percent_change = 2.0
+basin_maximum_change = 40.
+
+# baseline irrigation area
+baseline_year = 2015
+baseline_irrigation_area = irrigated_area_05min_hectar_meier_g_aei_ssp5_2015_2100.nc
+
+# future irrigation area
+original_future_irrigation_area =irrigated_area_05min_hectar_meier_g_aei_ssp5_2015_2100.nc
+
