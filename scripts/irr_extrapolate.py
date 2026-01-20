0d72284c6a2e7940cd9ecf6c4dbff081cad7171e
Edwin Sutanudjaja <edwinkost@heol>
2026-01-20 16:32:37 +0100

    start testing

--- scripts/irr_extrapolate.py
+++ scripts/irr_extrapolate.py
@@ -95,4 +95,4 @@ for year in range(2016, 2100, 1):
     print(year)
     print(float(check_basin_increase_in_percent))
     
-    pcr.aguila(final_current_year_irr_area)
+    # ~ pcr.aguila(final_current_year_irr_area)
