f2337e85365af9a4f81c5af4235017208d213cbd
U-SOLISCOM\Sutan101 <Sutan101@UU105378.soliscom.uu.nl>
2026-01-20 12:50:44 +0100

    in progress

--- scripts/irr_extrapolate.py
+++ scripts/irr_extrapolate.py
@@ -1,12 +1,84 @@
 
-basin_map = 
-basin_percent_change = 2.0
-basin_maximum_change = 40.
+import pcraster as prc
+import virtualOS as vos
 
-# baseline irrigation area
+# clone map at 30sec resolution
+clone_map = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_arise/develop/europe_30sec/cloneMaps/clonemaps_europe_countries/rhinemeuse/rhinemeuse_30sec.map"
+pcr.setclone(clone_map)
+
+# use directly the bounding box as the 'basin' map (area of interest) 
+basin_map = pcr.nominal(1.0)
+
+# percentange increse change
+basin_annual_percent_change = 2.0
+basin_maximum_percent_change = 40.
+
+# cell area
+cell_area_in_m2_file
+# - cell area in hectar
+cell_area  = 
+
+# original future irrigation area (based on PCR-GLOBWB aqueduct input files)
+original_future_irrigation_area_file = "/home/jsteyaert1/rhine_30sec/ssp5_2015_2100/irrigated_area_30sec_hectar_meier_g_aei_ssp5_2015_2100_v20250310.nc"
+
+# baseline irrigation area (hectar)
 baseline_year = 2015
-baseline_irrigation_area = irrigated_area_05min_hectar_meier_g_aei_ssp5_2015_2100.nc
+baseline_irrigation_area_file = original_future_irrigation_area_file
+baseline_irrigation_area = vos.netcdf2PCRobjClone(ncFile = baseline_irrigation_area_file,\
+                                                  varName = "automatic", dateInput = str(baseline_year)+"01-01", useDoy = None, cloneMapFileName  = clone_map, LatitudeLongitude = True, specificFillValue = None)
 
-# future irrigation area
-original_future_irrigation_area =irrigated_area_05min_hectar_meier_g_aei_ssp5_2015_2100.nc
+# basin scale maximum irrigation area (hectar) 
+basin_maximum_irr_area = pcr.areatotal(baseline_irrigation_area, basin_map) * (1. + basin_maximum_percent_change/100.)
 
+for year in range(2016, 2100, 1):
+    
+    # calculate all (or most) of the following steps in hectare
+    
+    # get the previous year of irrigated area
+    if year == 2016:
+        previous_year_irr_area = baseline_irrigation_area
+    else:
+        previous_year_irr_area = baseline_irrigation_area
+
+    # get basin scale irrigation area for this year
+    basin_previous_year_irr_area = pcr.areatotal(previous_year_irr_area, basin_map)
+    basin_irr_area = basin_previous_year_irr_area * (1. + basin_annual_percent_change/100.)
+    # - limited by basin scale maximum irrigation area
+    basin_irr_area = pcr.min(basin_maximum_irr_area, basin_irr_area) 
+
+    # get basin_increase 
+    basin_irr_area = pcr.max(0.0, basin_irr_area - basin_previous_year_irr_area)
+    
+    # get the pixel scale estimate of increase based on the original future irrigation area (based on PCR-GLOBWB aqueduct input files)
+    prev_year_estimate     = vos.netcdf2PCRobjClone(ncFile = original_future_irrigation_area_file,\
+                                                    varName = "automatic", dateInput = str(year-1)+"01-01", useDoy = None, cloneMapFileName = clone_map, LatitudeLongitude = True, specificFillValue = None) 
+    current_year_estimate  = vos.netcdf2PCRobjClone(ncFile = original_future_irrigation_area_file,\
+                                                    varName = "automatic", dateInput = str(year)+"01-01", useDoy = None, cloneMapFileName = clone_map, LatitudeLongitude = True, specificFillValue = None)
+    
+    # - pixel scale estimate (hectar)
+    delta_increase = pcr.max(0.0, current_year_estimate - prev_year_estimate)
+
+    # - correct it to basin_increase_in_percent and distribute it to each pixel (in hectar)
+    delta_increase = pcr.max(delta_increase, basin_increase_in_percent * previous_year_irr_area) 
+    basin_delta_increase = pcr.areatotal(delta_increase, basin_map)
+    delta_increase = (delta_increase / basin_delta_increase) * basin_increase_in_percent * basin_previous_year_irr_area
+
+    # estimate the current irrigation area
+    estimate_current_year_irr_area =  previous_year_irr_area + delta_increase
+    
+    # constrained by cell area
+    check_area, valid = pcr.cellvalue(pcr.mapmaximum(pcr.ifthenelse(estimate_current_year_irr_area > cell_area, pcr.scalar(1.0), pcr.scalar(0.0))), 1)
+    while check_area > 0:
+        # - limit the increase to cell area
+        delta_increase = pcr.max(0.0, cell_area – delta_increase)
+        
+        basin_delta_increase = pcr.areatotal(delta_increase, basin_map)
+        delta_increase = (delta_increase / basin_delta_increase) * basin_increase_in_percent * basin_previous_year_irr_area
+        
+        estimate_current_year_irr_area = previous_year_irr_area + delta_increase
+        check_area, valid = pcr.cellvalue(pcr.mapmaximum(pcr.ifthenelse(estimate_current_year_irr_area > cell_area, pcr.scalar(1.0), pcr.scalar(0.0))), 1)
+    
+    final_current_year_irr_area = estimate_current_year_irr_area
+    
+
+    
